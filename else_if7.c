b4c42f814964d04081bf524b3effa79380f76923
Sahilbelim <106684275+Sahilbelim@users.noreply.github.com>
2023-10-06 18:00:26 +0530

    zodiac sign

---
+++ else_if7.c
@@ -0,0 +1,137 @@
+#include<stdio.h>
+void main()
+{
+    int date, month;
+
+    printf("Enter Value of  Date : ");
+    scanf("%d", &date);
+    printf("Enter Value of month : ");
+    scanf("%d", &month);
+
+    printf("Value of Date : %d and Value of month %d \n\n", date, month);
+
+    if (month==3 && date>=21 && date<=31)
+    {
+        printf("Symbol : The Ram and Zodiac sign : Aries \n");
+    }
+    else if (month==4 && date>=1 && date<=19)
+    {
+        printf("Symbol : The Ram and Zodiac sign : Aries \n");
+   
+    }
+    else if (month==4 && date>=20 && date<=30)
+    {
+        printf("Symbol : The Bull and Zodiac sign : Taurus \n");
+   
+    }
+    else if (month==5 && date>=1 && date<=20)
+    {
+        printf("Symbol : The Bull and Zodiac sign : Taurus \n");
+    }
+    else if (month==5 && date>=21 && date<=31)
+    {
+        printf("Symbol : The twins and Zodiac Sign : Gemini \n");
+    }
+    
+    else if (month==6 && date>=1 && date<=20)
+    {
+        printf("Symbol : The twins and Zodiac Sign : Gemini \n");
+    }
+    
+    else if (month==6 && date>=21 && date<=30)
+    {
+        printf("Symbol : The Crabe and Zodiac Sign : Cencer \n");
+    }
+    
+    else if (month==7 && date>=1 && date<=22)
+    {
+        printf("Symbol : The Crabe and Zodiac Sign : Cencer \n");
+    }
+    
+    else if (month==7 && date>=23 && date<=31)
+    {
+        printf("Symbol : The Lion and Zodiac Sign : Leo \n");
+    }
+    
+    else if (month==8 && date>=1 && date<=22)
+    {
+        printf("Symbol : The Lion and Zodiac Sign : Leo \n");
+    }
+    
+    else if (month==8 && date>=23 && date<=31)
+    {
+        printf("Symbol : The virgin and Zodiac Sign : Virgo \n");
+    }
+    
+    else if (month==9 && date>=1 && date<=22)
+    {
+        printf("Symbol : The virgin and Zodiac Sign : Virgo \n");
+    }
+    
+    else if (month==9 && date>=23 && date<=30)
+    {
+        printf("Symbol : The Scales and Zodiac Sign : Libra \n");
+    }
+    
+    else if (month==10 && date>=1 && date<=22)
+    {
+        printf("Symbol : The Scales and Zodiac Sign : Libra \n");
+    }
+    
+    else if (month==10 && date>=23 && date<=31)
+    {
+        printf("Symbol : The scopion and Zodiac Sign : Scorpio \n");
+    }
+    
+    else if (month==11 && date>=1 && date<=21)
+    {
+        printf("Symbol : The scopion and Zodiac Sign : Scorpio \n");
+    }
+    
+    else if (month==11 && date>=22 && date<=30)
+    {
+        printf("Symbol : The Archer and Zodiac Sign : Sagittiarius \n");
+    }
+    
+    else if (month==12 && date>=1 && date<=21)
+    {
+        printf("Symbol : The Archer and Zodiac Sign : Sagittiarius \n");
+    }
+    
+    else if (month==12 && date>=22 && date<=31)
+    {
+        printf("Symbol : The Goat and Zodiac Sign : Capricorn \n");
+    }
+    
+    else if (month==1 && date>=1 && date<=19)
+    {
+        printf("Symbol : The Goat and Zodiac Sign : Capricorn \n");
+    }
+    
+    else if (month==1 && date>=20 && date<=31)
+    {
+        printf("Symbol : The Water Bearer and Zodiac Sign : Aqurius \n");
+    }
+    
+    else if (month==2 && date>=1 && date<=18)
+    {
+        printf("Symbol : The Water Bearer and Zodiac Sign : Aqurius \n");
+    }
+    
+    else if (month==2 && date>=19 && date<=29)
+    {
+        printf("Symbol : The Fishes and Zodiac Sign : pisces \n");
+    }
+    
+    else if (month==3 && date>=1 && date<=20)
+    {
+        printf("Symbol : The Fishes and Zodiac Sign : pisces \n");
+    }
+    else
+    {
+        printf("Invalide Date and month ");
+    }
+        
+    
+    
+}
