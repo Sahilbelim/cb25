e83f10339118f0fdf821de460344e64cc99dd173
Sahilbelim <106684275+Sahilbelim@users.noreply.github.com>
2023-10-19 17:59:20 +0530

    do while

--- do_while4.c
+++ do_while4.c
@@ -1,19 +1,23 @@
 //write a program to findout whether 3 digit number is armstrong number or not
 //input 153
 // 1,5,3 (1*1*1) + (5*5*5) + (3 * 3 * 3)
-//output = 153
+//output = 153 /10 =15%10 //5 153/100
 #include <stdio.h>
 void main()
 {
     int amount = 0;
     int reminder;
     int reverse = 0;
+    int last;
+
     printf("enter amount");
     scanf("%d", &amount);
 
-    reminder = amount % 10; //3
-    reminder = reminder * reminder * reminder;
+    last = amount % 10; //3
+
+    reminder = (reminder * reminder * reminder);
     reverse = reverse + reminder;
+    
     amount = amount / 10;
 
     printf("\n reverse = %d", reverse);
---
+++ do_while5.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+void main()
+{
+    int choise=0;
+    float number1, number2, Ans;
+do
+  { 
+     printf("\n\n|||||||||||||||||||||||||||||Select number for perform oparetion : ||||||||||||||||||||||||||\n\n ");
+    printf("1 for Addition \n ");
+    printf("2 for Subtraction  \n ");
+    printf("3 for Muliplication  \n ");
+    printf("4 for Division  \n ");
+    printf("Select Number : ");
+    scanf("%d", &choise);
+    if(choise>=1 && choise<=4)
+    {
+    printf("Eneter Value of  Number 1 :");
+    scanf("%f", &number1);
+    printf("Eneter Value of  Number 2 :");
+    scanf("%f", &number2);
+    }
+    switch (choise)
+    {
+    case 1:
+        Ans = number1 + number2;
+        printf("Addition of Given Numbers : %.2f\n", Ans);
+        break;
+    case 2:
+        Ans = number1 - number2;
+        printf("Subtraction of Given Numbers : %.2f\n", Ans);
+        break;
+    case 3:
+        Ans = number1 * number2;
+        printf("Multiplication  of Given Numbers : %.2f\n", Ans);
+        break;
+    case 4:
+        Ans = number1 / number2;
+        printf("Division  of Given Numbers : %.2f\n", Ans);
+        break;
+
+    default:
+        printf("Invalide Choise\n");
+        break;
+    }
+
+    printf("If continue Then Enter 0 -->> ");
+    scanf("%d", &choise);
+
+
+  } while (choise==0);
+}
